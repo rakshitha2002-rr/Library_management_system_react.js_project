a7b4e993dbe4acb9ab191fd12e32ecd6ee4887f8
rakshitha2002-rr <raikarrakshitha6@gmail.com>
2026-02-28 23:17:45 +0530

    first commit

--- src/app/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-export const apiSlice = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://library-management-api-mauve.vercel.app/api" }),
-  tagTypes: ["Books", "Borrows"],
-  endpoints: () => ({}),
-});
